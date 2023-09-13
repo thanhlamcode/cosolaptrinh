91415d14cf7ae01e9d72910dde11237a649d775e
Aritannia <ngophucnguyen1976@gmail.com>
2023-09-13 23:12:41 +0700

    Cải tiến Nhap_Vao, Tiếng việt, giao diện

--- DangNhap/DangNhap/Program.cs
+++ DangNhap/DangNhap/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using DangNhap;
+using System.Linq.Expressions;
 
 namespace Login
 {
@@ -12,6 +13,8 @@ namespace Login
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
             //AttachDbFilename thay bằng đường dẫn được lưu ở máy bạn để có thể hoạt động được
             string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\84967\source\repos\thanhlamcode\DangNhap\DangNhap\Project_DB.mdf;Integrated Security=True";
 
@@ -20,14 +23,24 @@ namespace Login
                 connection.Open();
 
                 char accessauthority = ' ';
+
                 // Chọn đăng nhập, đăng ký và thoát
-                int choose1 = Rogin.Nhap_Vao(1, 3);
+                // Tham số thứ hai sử dụng Lambda - Kiểu delegate: dùng để lưu các tham chiêu method.
+                // Lambda cũng là kiểu Anonymous Type: kiểu không có tên, trong TH này là một method không tên.
+                // n bắt đầu là một tham số, sau dấu => sẽ thực thi code
+
+                string noti1 = "1.Đăng nhập.\n2.Đăng Ký.\n3.Exit.\nNhập số (1 -> 3): ";
+                int choose1 = Rogin.Nhap_Vao<int>(noti1, n => n >= 1 && n <= 3);
                 Rogin.First_Move(connection, ref accessauthority, choose1);
 
                 // Vai trò Admin Lọc user ảo, user rác
                 if(accessauthority == 'A')
                 {
-                    int choose2 = Rogin.Nhap_Vao(1, 5);
+                    string noti2 = "1.Delete user dựa trên số lần đăng nhập." +
+                        "\n2.Delete user dựa trên số lần bị report." + "\n3.Delete user dựa trên số lần comment." +
+                        "\n4. Delete user dựa trên userId.";
+
+                    int choose2 = Rogin.Nhap_Vao<int>(noti2, n => n >= 1 && n <= 5);
                     Admin.Loc_Rac(connection, choose2);
                     // Vai trò Admin tiếp tục trong if này
                 }    
--- DangNhap/DangNhap/Rogin.cs
+++ DangNhap/DangNhap/Rogin.cs
@@ -20,32 +20,7 @@ namespace DangNhap
                     break;
 
                 case 2: //Register
-                    int biggest_num = Biggest_id(connection);
-                    try
-                    {
-                        for (int i = 100; i <= biggest_num; i++)
-                        {
-                            // Nếu có accountId vào trống ví dụ như ta có trong bảng accountId 100, 101, 104, 105
-                            // Thì for sẽ lặp qua từ 100 -> 105, nếu đến giá trị 102 mà nó không tìm thấy
-                            // trong table thì nó sẽ tạo một accountId mới trong bảng với giá trị là 102 rồi chèn các data vào
-                            if (IsEmptyNumber_Namesake(connection, i, ""))
-                            {
-                                Register(connection, i);
-                                break;
-                            }
-                            // Nếu không tìm thấy giá trị trống nào giữa các dòng data thì nó sẽ tự động chèn xuống dưới vị trí bên dưới còn trống
-                            else if ((i + 1) > biggest_num)
-                            {
-                                Register(connection, i + 1);
-                            }
-                        }
-                        Console.WriteLine("Success Register.");
-                    }
-                    catch (Exception loi)
-                    {
-                        Console.WriteLine("False Register. ");
-                        Console.WriteLine("Error Main Register: {0}", loi.Message);
-                    }
+                    RegisterInterface(connection);
                     break;
 
                 case 3://Exit
@@ -53,32 +28,6 @@ namespace DangNhap
                     return;
             }
         }
-        public static int Nhap_Vao(int a, int b)
-        {
-            //Kiểm tra giá trị số nhập vào
-            int n;
-            while (true)
-            {
-                Console.Write($"Enter the number ({a} -> {b}): ");
-                if (int.TryParse(Console.ReadLine(), out n) && Dieukien(a, b, n))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid Value. Please try again");
-                }
-            }
-
-            return n;
-        }
-
-        public static bool Dieukien(int a, int b, int n)
-        {
-            if (n >= a && n <= b) return true;
-            else return false;
-        }
-
         public static char Login(SqlConnection connection)
         {
             char accessauthority = ' ';
@@ -109,20 +58,12 @@ namespace DangNhap
                             {
                                 // Để xác định vai trò Admin hay User và dừng vòng lặp trong khi
                                 // đã kiểm tra đúng TK và MK
-                                if (ar == true)
-                                {
-                                    accessauthority = 'A';
-                                    break;
-                                }
-                                else
-                                {
-                                    accessauthority = 'U';
-                                    break;
-                                }
+                                accessauthority = ar ? 'A' : 'U';
                             }
 
 
                         }
+                        Console.Clear();
                         sqlreader.Close();
 
                         // Nếu đúng thì ngừng vòng lặp ngoài, sai thì thông báo nhập lại
@@ -138,7 +79,127 @@ namespace DangNhap
 
             return accessauthority;
         }
+        public static void RegisterInterface(SqlConnection connection)
+        {
+            int biggest_num = Biggest_id(connection);
+            try
+            {
+                for (int i = 100; i <= biggest_num; i++)
+                {
+                    // Nếu có accountId vào trống ví dụ như ta có trong bảng accountId 100, 101, 104, 105
+                    // Thì for sẽ lặp qua từ 100 -> 105, nếu đến giá trị 102 mà nó không tìm thấy
+                    // trong table thì nó sẽ tạo một accountId mới trong bảng với giá trị là 102 rồi chèn các data vào
+                    if (IsEmptyNumber_Namesake(connection, i))
+                    {
+                        Register(connection, i);
+                        break;
+                    }
+                    // Nếu không tìm thấy giá trị trống nào giữa các dòng data thì nó sẽ tự động chèn xuống dưới vị trí bên dưới còn trống
+                    else if ((i + 1) > biggest_num)
+                    {
+                        Register(connection, i + 1);
+                    }
+                }
+                Console.WriteLine("Success Register.");
+            }
+            catch (Exception loi)
+            {
+                Console.WriteLine("False Register. ");
+                Console.WriteLine("Error Main Register: {0}", loi.Message);
+            }
+        }
+        public static void Register(SqlConnection connection, int number)
+        {
+            // Chèn giá trị mới vào các cột bằng các tham số truyền vào như
+            // @id, @tk, @mk,...
+            string query = "insert into Account (accountId, username, password, logintime, reporttime, commenttime, accessright) " +
+                "values (@id, @tk, @mk, @lgt, @rt, @ct, @ar)";
 
+            using (var command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    command.Parameters.AddWithValue("@id", number);
+
+                    //kiểm tra xem username có trùng không
+                    var userten = command.Parameters.AddWithValue("@tk",
+                        Nhap_Vao<string>("Username: ", s => string.IsNullOrEmpty(s), s => IsEmptyNumber_Namesake(connection, 0, s)));
+
+                    Console.Write("Password: ");
+                    command.Parameters.AddWithValue("@mk", Console.ReadLine());
+                    command.Parameters.AddWithValue("@lgt", 1);
+                    command.Parameters.AddWithValue("@rt", 0);
+                    command.Parameters.AddWithValue("@ct", 0);
+                    command.Parameters.AddWithValue("@ar", false);
+
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception loi)
+                {
+                    Console.WriteLine("Error Register: {0}", loi.Message);
+                }
+
+            }
+        }
+        // T Nhap_Vao <T> Nhap_Vao<T>: T phía trước: giá trị generic cho phép làm việc với kiểu data xác định ngay thời điểm biên dịch
+        // T phía sau: giá trị trả về của tham số
+
+        // Func<T, bool> dieukien : Hàm delegate trả về giá trị bool.
+
+        // bool> Name_Same = null : tham số có giá trị mặc định là null, có nghĩa rằng ko cần kiểm tra điều kiện trong tham số
+        // này nếu không muốn.
+
+        // T result = (T)Convert.ChangeType(input, typeof(T)); -> Ép kiểu hai lần do C.C lần đầu trả về kiểu object.
+        // bên trong đổi giá trị kiểu data của input sang typeof(T)
+        public static T Nhap_Vao<T>(string text, Func<T, bool> dieukien, Func<T, bool> Name_Same = null)
+        {
+            T result;
+            bool convert1 = false;
+            while (true)
+            {
+                Console.Write(text);
+                string input = Console.ReadLine();
+                try
+                {
+                    try
+                    {
+                        // Thử check kiểu nếu lỗi, không thể chuyển thì sẽ mặc định false.
+                        // result = default(T); : đảm bảo luôn có một giá trị hợp lệ khi ép kiểu ko thành công
+                        result = (T)Convert.ChangeType(input, typeof(T));
+                        convert1 = true;
+                    }
+                    catch 
+                    {
+                        result = default(T);
+                    }
+                    // Kiểm tra TH nếu nhập vào username: KT phần Func dieukien -> Nếu trống thì nhảy vào.
+                    // Bên trong kiểm tra nếu tham số 3 không để trống và Kiểm tra điều kiện trong Func Name_Same
+                    // liệu tên có trùng không. Nếu có suy ra else không thì trả về giá trị result.
+
+                    // Kiểm tra TH nếu nhập vào số: KT phần Func dieukien -> Nếu thỏa trong khoảng đó thì nhảy vào
+                    // Kiểm tra liệu tham số 3 có null không nếu null nhảy vào không thì xét tiếp
+                    if (convert1 && dieukien(result))
+                    {
+                        if (Name_Same == null || (Name_Same != null && Name_Same(result)))
+                        {
+                            Console.Clear();
+                            return result;
+                        }
+                        else Console.WriteLine("Giá trị bạn nhập đã tồn tại. Vui lòng thử tên khác.");
+
+                    }
+                    else 
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Giá trị không hợp lệ. Vui lòng thử lại."); 
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error Nhap_Vao: {e}");
+                }
+            }
+        }
         public static int Biggest_id(SqlConnection connection)
         {
             int num = 0;
@@ -156,8 +217,7 @@ namespace DangNhap
             }
             return num;
         }
-
-        public static bool IsEmptyNumber_Namesake(SqlConnection connection, int number, string ten)
+        public static bool IsEmptyNumber_Namesake(SqlConnection connection, int number, string ten = null)
         {
             // Đếm số lượng record có trong Table Account với điều kiện giá trị cột accountId phải trùng với 
             // tham số truyền vào @id
@@ -183,58 +243,6 @@ namespace DangNhap
 
                 return count == 0;
             }
-        }
-
-        public static void Register(SqlConnection connection, int number)
-        {
-            // Chèn giá trị mới vào các cột bằng các tham số truyền vào như
-            // @id, @tk, @mk,...
-            string query = "insert into Account (accountId, username, password, logintime, reporttime, commenttime, accessright) " +
-                "values (@id, @tk, @mk, @lgt, @rt, @ct, @ar)";
-
-            using (var command = new SqlCommand(query, connection))
-            {
-                try
-                {
-                    command.Parameters.AddWithValue("@id", number);
-
-                    //kiểm tra xem username có trùng không
-                    var userten = command.Parameters.AddWithValue("@tk", Check_Name(connection));
-
-                    Console.Write("Password: ");
-                    command.Parameters.AddWithValue("@mk", Console.ReadLine());
-                    command.Parameters.AddWithValue("@lgt", 1);
-                    command.Parameters.AddWithValue("@rt", 0);
-                    command.Parameters.AddWithValue("@ct", 0);
-                    command.Parameters.AddWithValue("@ar", false);
-
-                    command.ExecuteNonQuery();
-                }
-                catch (Exception loi)
-                {
-                    Console.WriteLine("Error Register: {0}", loi.Message);
-                }
-
-            }
-        }
-
-        public static string Check_Name(SqlConnection connection)
-        {
-            // Function này dùng nhằm kiểm tra xem username có bị trùng không, nếu trùng thì bắt nhập lại
-            string num = " ";
-            while (true)
-            {
-                Console.Write("Username: ");
-                num = Console.ReadLine();
-                if (IsEmptyNumber_Namesake(connection, 0, num))
-                {
-                    return num;
-                }
-                else
-                {
-                    Console.WriteLine("The name you entered already exists. Please try the other name.");
-                }
-            }
-        }
+        }   
     }
 }
