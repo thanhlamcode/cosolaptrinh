0b8e3b8d06c9e70e6dcd98244984f7164bb8d61d
Hiu11 <144320948+Hiu11@users.noreply.github.com>
2023-09-24 13:43:57 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Film3
+namespace _4
 {
     internal class Program
     {
+        static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\source\repos\thanhlamcode\DangNhap\DangNhap\Project_DB.mdf;Integrated Security=True";
+
         static void Main(string[] args)
         {
-            //chuỗi kết nối đến csdl
-            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Asus\source\repos\thanhlamcode\DangNhap\DangNhap\Project_DB.mdf;Integrated Security=True";
-            //tạo kết nối tới csdl
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.Unicode;
+
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                //tạo đối tượng Sqlcommand
-                using (var command = connection.CreateCommand())
-                {
-                    //thiết lập câu truy vấn sql (ở đây dùng ORDER BY để xếp rating giảm dần)
-                    command.CommandText = "Select filmid, tenphim, theloai, nhasanxuat, sotap, luotxem, doanhthu, rating FROM MOVIE WHERE filmid >= @filmid ORDER BY rating DESC";
-                    //thiết lập tham số 
-                    var FilmId = command.Parameters.AddWithValue("@filmid", 5);
-                    FilmId.Value = 0;
-                    //thực thi câu truy vấn
-                    var sqlreader = command.ExecuteReader();
-                    //kiểm tra có trả về dữ liệu k ?
-                    if (sqlreader.HasRows)
-                    {
 
-                        Console.WriteLine("Xep hang cac bo phim :");
-                        //đọc kq và in ra 
-                        while (sqlreader.Read())
+                while (true)
+                {
+                    Console.WriteLine("==== Chương trình quản lý báo cáo bình luận ====");
+                    Console.WriteLine("1. Thêm báo cáo bình luận");
+                    Console.WriteLine("2. Đổi mật khẩu người dùng");
+                    Console.WriteLine("0. Thoát chương trình");
+                    Console.WriteLine("==============================================");
+
+                    Console.Write("Vui lòng chọn một tùy chọn: ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+
+                    switch (option)
+                    {
+                        case 1:
+                            AddCommentReport(connection);
+                            break;
+                        case 2:
+                            ChangeUserPassword(connection);
+                            break;
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.WriteLine("Lựa chọn không hợp lệ. Vui lòng thử lại.");
+                            break;
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void AddCommentReport(SqlConnection connection)
+        {
+            Console.WriteLine("==== Thêm báo cáo bình luận ====");
+            Console.Write("Nhập tên người dùng (username): ");
+            string username = Console.ReadLine();
+
+            // Kiểm tra xem người dùng có tồn tại trong cơ sở dữ liệu hay không
+            string checkQuery = "SELECT COUNT(*) FROM Account WHERE Username = @Username";
+            using (var command = new SqlCommand(checkQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Username", username);
+                int accountCount = (int)command.ExecuteScalar();
+
+                if (accountCount > 0)
+                {
+                    // Tăng giá trị ReportTime trong bảng Account
+                    string updateQuery = "UPDATE Account SET ReportTime = ReportTime + 1 WHERE Username = @Username";
+                    using (var updateCommand = new SqlCommand(updateQuery, connection))
+                    {
+                        updateCommand.Parameters.AddWithValue("@Username", username);
+                        updateCommand.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine("Báo cáo bình luận thành công.");
+                }
+                else
+                {
+                    Console.WriteLine("Người dùng không tồn tại.");
+                }
+            }
+        }
+        static void ChangeUserPassword(SqlConnection connection)
+        {
+            Console.WriteLine("==== Đổi mật khẩu người dùng ====");
+            Console.Write("Nhập tên người dùng (username): ");
+            string username = Console.ReadLine();
+
+            // Kiểm tra xem người dùng có tồn tại trong cơ sở dữ liệu hay không
+            string checkUserQuery = "SELECT COUNT(*) FROM Account WHERE Username = @Username";
+            using (var checkUserCommand = new SqlCommand(checkUserQuery, connection))
+            {
+                checkUserCommand.Parameters.AddWithValue("@Username", username);
+                int userCount = (int)checkUserCommand.ExecuteScalar();
+
+                if (userCount > 0)
+                {
+                    Console.Write("Nhập mật khẩu cũ: ");
+                    string oldPassword = Console.ReadLine();
+
+                    // Kiểm tra mật khẩu cũ có khớp với người dùng hay không
+                    string checkPasswordQuery = "SELECT COUNT(*) FROM Account WHERE Username = @Username AND Password = @OldPassword";
+                    using (var checkPasswordCommand = new SqlCommand(checkPasswordQuery, connection))
+                    {
+                        checkPasswordCommand.Parameters.AddWithValue("@Username", username);
+                        checkPasswordCommand.Parameters.AddWithValue("@OldPassword", oldPassword);
+                        int passwordCount = (int)checkPasswordCommand.ExecuteScalar();
+
+                        if (passwordCount > 0)
                         {
-                            var id = sqlreader.GetInt32(0);
-                            var ten = sqlreader["tenphim"];
-                            var ranked = sqlreader["rating"];
-                            var lxem = sqlreader.GetInt32(6);
-                            Console.WriteLine($"rating: {ranked} - {ten} ({lxem} luot xem)");
+                            Console.Write("Nhập mật khẩu mới: ");
+                            string newPassword = Console.ReadLine();
+
+                            // Cập nhật mật khẩu mới cho người dùng
+                            string updateQuery = "UPDATE Account SET Password = @NewPassword WHERE Username = @Username";
+                            using (var updateCommand = new SqlCommand(updateQuery, connection))
+                            {
+                                updateCommand.Parameters.AddWithValue("@NewPassword", newPassword);
+                                updateCommand.Parameters.AddWithValue("@Username", username);
+                                int rowsAffected = updateCommand.ExecuteNonQuery();
+
+                                if (rowsAffected > 0)
+                                {
+                                    Console.WriteLine("Đổi mật khẩu thành công.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Đổi mật khẩu không thành công.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Mật khẩu cũ không chính xác.");
                         }
                     }
-                    else
-                    {
-                        Console.WriteLine("Khong co du lieu phim nay");
-                    }
-                    Console.WriteLine("Ban muon xem bo phim nao ?");
                 }
-                //đóng kết nối
-                connection.Close();
+                else
+                {
+                    Console.WriteLine("Người dùng không tồn tại.");
+                }
             }
         }
     }
