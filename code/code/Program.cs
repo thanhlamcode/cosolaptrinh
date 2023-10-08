32864b2e0ceb9af4dd351cc720594c77d122de89
thanhlamcode <lamdoan1122334455@gmail.com>
2023-10-08 15:35:57 +0700

    ok

---
+++ code/code/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            support.Menuchonlua();
+        }
+    }
+}
