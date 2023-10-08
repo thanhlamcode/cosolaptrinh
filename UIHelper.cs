419b9b24dd3d9b2f63d31d09719ed74e73db3333
ThanhNhan291204 <144352996+ThanhNhan291204@users.noreply.github.com>
2023-10-08 11:33:32 +0700

    Add files via upload

---
+++ UIHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyProgram
+{
+    public static class UIHelper
+    {
+        public static void giaodien()
+        {
+            Console.WriteLine("╔══════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║                   Ứng dụng Quản lý Đánh giá              ║");
+            Console.WriteLine("╟──────────────────────────────────────────────────────────╢");
+            Console.WriteLine("║ 1. Hiển thị danh sách cmtid                              ║");
+            Console.WriteLine("║ 2. Xoá comment theo Account ID                           ║");
+            Console.WriteLine("║ 3. Thoát                                                 ║");
+            Console.WriteLine("╟──────────────────────────────────────────────────────────╢");
+            Console.WriteLine("║                  Hãy chọn một tùy chọn (1-3):            ║");
+            Console.WriteLine("╚══════════════════════════════════════════════════════════╝");
+        }
+    }
+}
