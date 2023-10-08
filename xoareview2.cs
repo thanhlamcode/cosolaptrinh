964d1a70727ae922e78850fa769cd08791ec830d
ThanhNhan291204 <144352996+ThanhNhan291204@users.noreply.github.com>
2023-10-08 16:07:33 +0700

    Delete xoareview2.cs

--- xoareview2.cs
+++
@@ -1,5 +0,0 @@
-﻿Build started...
-1>------ Build started: Project: ConsoleApp2, Configuration: Debug Any CPU ------
-1>  ConsoleApp2 -> C:\Users\MyPC\Downloads\ConsoleApp2\bin\Debug\ConsoleApp2.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build started at 12:50 and took 00,236 seconds ==========
