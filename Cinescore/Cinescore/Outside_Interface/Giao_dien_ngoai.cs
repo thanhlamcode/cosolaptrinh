bfc023648e1ac9fbf878a0349c9ed7889771a9c2
Aritannia <ngophucnguyen1976@gmail.com>
2023-10-08 20:45:34 +0700

    Hiệu chỉnh xong phần UI bên ngoài

---
+++ Cinescore/Cinescore/Outside_Interface/Giao_dien_ngoai.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinescore
+{
+    internal class Giao_dien_ngoai
+    {
+    }
+}
