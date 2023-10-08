93d0f0a2cca3eea9234988a9c5054f0c8ae21e83
thanhlamcode <lamdoan1122334455@gmail.com>
2023-10-08 21:14:07 +0700

    add

--- Cinescore/Cinescore/Admin/menu.cs
+++ Cinescore/Cinescore/Admin/menu.cs
@@ -1,19 +1,18 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Outside_Interface
+namespace Admin
 {
-    internal class Menu
+    class Menu
     {
         private string Prompts;
         private string[] Options;
         private int SelectedIndex;
         private bool Flag;
 
-        // Khởi tạo một constructor -> Thuận tiện khi tạo một object
         public Menu(string[] options, string prompts, bool flag = true)
         {
             Prompts = prompts;
@@ -22,11 +21,9 @@ namespace Outside_Interface
             SelectedIndex = 0;
         }
 
-        // Hàm chạy các lựa chọn
         private void Display()
         {
             int num_title = 2;
-            int max = 0;
             if (!Flag)
             {
                 Print_title();
@@ -34,25 +31,20 @@ namespace Outside_Interface
             }
             else Print_Prompts();
 
-            // In ra dòng có độ dài lớn nhất, làm chuẩn cho việc in ra khung
-            for (int i = 0; i < Options.Length; i++)
-            {
-                if (Options[i].Length > max) max = Options[i].Length;
-                else continue;
-            }
-
             string text = null;
+            int t1 = (Console.WindowWidth - 60) / 2;
             int t2 = Prompts.Split('\n').Length + num_title;
-            Console.SetCursorPosition(0, t2);
-            Console.WriteLine('╔' + new string('═', 16 + max) + '╗');
+            Console.SetCursorPosition(t1, t2);
+            Console.WriteLine('+' + new string('-', 58) + '+');
 
             for (int i = 0; i < Options.Length; i++)
             {
                 text = Options[i];
                 char prefix;
-                Console.SetCursorPosition(0, t2 + 1 + i);
-                Console.Write("║");
-                Console.SetCursorPosition(6, t2 + 1 + i);
+                int mid_2 = (50 - (Options[i].Length + 8)) / 2;
+                Console.SetCursorPosition(t1, t2 + 1 + i);
+                Console.Write("|");
+                Console.SetCursorPosition(t1 + 6 + mid_2, t2 + 1 + i);
                 if (i == SelectedIndex)
                 {
                     prefix = '>';
@@ -65,40 +57,35 @@ namespace Outside_Interface
                     Console.ForegroundColor = ConsoleColor.White;
                     Console.BackgroundColor = ConsoleColor.Black;
                 }
-                Console.Write($"{new string(prefix, 2)}--{text}--");
-                Console.SetCursorPosition(17 + max, t2 + 1 + i);
+                Console.Write($"{new string(prefix, 2)}[{text}]");
+                Console.SetCursorPosition(t1 + 59, t2 + 1 + i);
                 Console.ResetColor();
-                Console.WriteLine("║");
+                Console.WriteLine("|");
             }
 
-            Console.SetCursorPosition(0, t2 + Options.Length + 1);
-            Console.WriteLine('╚' + new string('═', 16 + max) + '╝');
+            Console.SetCursorPosition(t1, t2 + Options.Length + 1);
+            Console.WriteLine('+' + new string('-', 58) + '+');
         }
 
-        // In ra một tiêu đề bình thường
-        private void Print_Prompts()
+        void Print_Prompts()
         {
-            int max = 0;
-            for (int i = 0; i < Prompts.Split('\n').Length; i++)
-            {
-                if (Prompts.Split('\n')[i].Length > max) max = Prompts.Split('\n')[i].Length;
-                else continue;
-            }
+            int t1 = (Console.WindowWidth - 60) / 2;
 
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine('╔' + new string('═', 10 + max) + '╗');
+            Console.SetCursorPosition(t1, 0);
+            Console.WriteLine('+' + new string('-', 58) + '+');
 
             for (int i = 0; i < Prompts.Split('\n').Length; i++)
             {
-                Console.SetCursorPosition(0, 1 + i);
-                Console.Write("║");
-                Console.SetCursorPosition(6, 1 + i);
+                int mid_2 = (50 - Prompts.Split('\n')[i].Length) / 2;
+                Console.SetCursorPosition(t1, 1 + i);
+                Console.Write("|");
+                Console.SetCursorPosition(t1 + 6 + mid_2, 1 + i);
                 Console.WriteLine($"{Prompts.Split('\n')[i]}");
-                Console.SetCursorPosition(11 + max, 1 + i);
-                Console.WriteLine("║");
+                Console.SetCursorPosition(t1 + 59, 1 + i);
+                Console.WriteLine("|");
             }
-            Console.SetCursorPosition(0, Prompts.Split('\n').Length + 1);
-            Console.WriteLine('╚' + new string('═', 10 + max) + '╝');
+            Console.SetCursorPosition(t1, Prompts.Split('\n').Length + 1);
+            Console.WriteLine('+' + new string('-', 58) + '+');
         }
 
         public int Run()
@@ -132,12 +119,12 @@ namespace Outside_Interface
             return SelectedIndex;
         }
 
-        // In ra các tiêu đề như: Logo UEH, Logo App, những thứ nhiều ký tự '\n'
         private void Print_title()
         {
+            int t1 = (Console.WindowWidth - 87) / 2;
             for (int i = 0; i < Prompts.Split('\n').Length; i++)
             {
-                Console.SetCursorPosition(0, i);
+                Console.SetCursorPosition(t1, i);
                 Console.WriteLine($"{Prompts.Split('\n')[i]}");
             }
         }
--- Cinescore/Cinescore/Outside_Interface/Check_Nhapvao.cs
+++ Cinescore/Cinescore/Outside_Interface/Check_Nhapvao.cs
@@ -4,9 +4,130 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cinescore
+namespace Outside_Interface
 {
     internal class Check_Nhapvao
     {
+        // Dùng để nhận một string nhập vào từ người dùng
+        public static string Themdulieu_string(string text)
+        {
+            string at1;
+            while (true)
+            {
+                Console.Write(text);
+                at1 = Console.ReadLine();
+                // Kiểm tra xem liệu user có nhập vào gì không hay là chỉ nhập khoảng trắng
+                // Nếu đúng như điều trên thì đưa đến else bắt nhập lại không thì trả về giá trị kiểu string
+                if (!string.IsNullOrEmpty(at1) && !string.IsNullOrWhiteSpace(at1))
+                {
+                    return at1;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Giá trị nhập vào không hợp lệ. Vui lòng nhập lại");
+                }
+            }
+        }
+
+        // Dùng để nhận một int nhập vào từ người dùng
+        public static int Themdulieu_int(string text)
+        {
+            int num;
+            while (true)
+            {
+                Console.Write(text);
+                // Kiểm tra liệu giá trị user nhập vào có phải là kiểu int và lớn hơn 0
+                // Nếu đúng điều trên thì trả về giá trị kiểu int không thì chạy qua else bắt nhập lại
+                if (int.TryParse(Console.ReadLine(), out num) && num > 0)
+                {
+                    return num;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Giá trị nhập vào không hợp lệ. Vui lòng nhập lại");
+                }
+            }
+        }
+
+        // Hàm dùng để chạy và đưa ra Id cuối cùng
+        // Kiểm tra nếu có khoảng trống số Id trong các trường dữ liệu
+        // Nếu không có thì tạo Id mới tiếp tục từ trường dữ liệu cuối cùng
+        public static string Check_Id(int rowC, List<List<string>> database)
+        {
+            int max = 0;
+            int c = 0;
+            int accountId = 0;
+            for (int i = 0; i < rowC; i++)
+            {
+                if (int.TryParse(database[i][0], out c))
+                {
+                    if (max < c) max = c;
+                }
+            }
+
+            for (int i = 100; i <= max; i++)
+            {
+                if (Have_Space(i, rowC, database))
+                {
+                    accountId = i;
+                    break;
+                }
+                else if ((i + 1) > max) accountId = i + 1;
+            }
+
+            return accountId.ToString();
+        }
+
+        // Kiểm tra liệu trong file có tồn tại trường dữ liệu trống hay không
+        // Ví dụ: từ 101 -> 105, trống 102, hàm Have_Space kiểm tra thấy điều đó -> Thực hiện trả về true
+        // Nếu không có khoảng trống nào -> Trả về false.
+        // Mục đích: Để kiểm tra nếu tồn tại khoảng trống giống với ví dụ trên -> Khi user tạo TK thì cho user cái ID trống đó
+        // Nếu không thì tạo Id mới tiếp tục từ trường dữ liệu cuối cùng.
+        private static bool Have_Space(int num_check, int rowC, List<List<string>> database)
+        {
+            int c = 0;
+            for (int i = 0; i < rowC; i++)
+            {
+                if (int.TryParse(database[i][0], out c))
+                {
+                    if (c == num_check) return false;
+                }
+            }
+            return true;
+        }
+
+        // Hàm để chạy Nhập user name khi hàm Dang_Ky được thực hiện
+        // Kiểm tra liệu user có điền gì vào username hoặc điền không trùng username không
+        // Nếu đúng thì hàm while sẽ được thoát ra và trả về kiểu string
+        // Không thì bắt nhập lại cho tới khi đúng
+        public static string Check_Same_Name(int rowC, List<List<string>> database)
+        {
+            string Username = " ";
+            do
+            {
+                Console.Clear();
+                if (!string.IsNullOrEmpty(Username) && Same_Name(Username, rowC, database))
+                {
+                    Console.WriteLine("Username đã tồn tại. Vui lòng nhập lại.");
+                }
+                Username = Themdulieu_string("Nhập vào Username: ");
+            } while (Same_Name(Username, rowC, database));
+
+            return Username;
+        }
+
+        // Kiểm tra liệu có trùng User_Name khi hàm Dang_Ky được thực hiện
+        // Nếu có thì trả về true -> Nếu không có thì trả về false
+        // Mục đích là bắt user nhập lại nếu trùng username khi Đăng Ký
+        private static bool Same_Name(string Username, int rowC, List<List<string>> database)
+        {
+            for (int i = 0; i < rowC; i++)
+            {
+                if (Username == database[i][1]) return true;
+            }
+            return false;
+        }
     }
 }
