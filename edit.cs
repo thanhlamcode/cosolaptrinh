f9aa4c290359befc895ee0ec2607a6f3f3521442
thanhlamcode <144249019+thanhlamcode@users.noreply.github.com>
2023-10-09 20:21:55 +0700

    Add files via upload

---
+++ edit.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    public class support
+    {
+        static void EditMovie(int filmid)
+        {
+            string[] lines = File.ReadAllLines(dataFilePath, Encoding.Unicode);
+            bool found = false;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split(',');
+                if (parts.Length >= 8)
+                {
+                    int existingFilmid;
+                    if (int.TryParse(parts[0], out existingFilmid) && existingFilmid == filmid)
+                    {
+                        found = true;
+                        Console.WriteLine("Thông tin hiện tại của bộ phim:");
+                        Console.WriteLine(new string('═', 60)); // In đường kẻ ngang
+                        Console.WriteLine();
+
+                        Print_Prompts("Film ID:", parts[0]);
+                        Print_Prompts("Tên phim:", parts[1]);
+                        Print_Prompts("Thể loại:", parts[2]);
+                        Print_Prompts("Nhà sản xuất:", parts[3]);
+                        Print_Prompts("Số tập:", parts[4]);
+                        Print_Prompts("Lượt xem:", parts[5]);
+                        Print_Prompts("Doanh thu:", parts[6]);
+                        Print_Prompts("Rating:", parts[7]);
+
+                        Console.WriteLine();
+                        Console.WriteLine(new string('═', 60)); // In đường kẻ ngang
+                        Console.WriteLine("Nhập các thông tin mới:");
+
+                        string tenphim, theloai, nhasanxuat;
+                        int sotap, luotxem, doanhthu;
+                        double rating;
+
+                        // Nhập thông tin mới với các hàm Print_Prompts
+                        tenphim = InputWithPrompt("Tên phim: ");
+                        theloai = InputWithPrompt("Thể loại: ");
+                        nhasanxuat = InputWithPrompt("Nhà sản xuất: ");
+                        sotap = IntInputWithPrompt("Số tập: ");
+                        luotxem = IntInputWithPrompt("Lượt xem: ");
+                        doanhthu = IntInputWithPrompt("Doanh thu: ");
+                        rating = DoubleInputWithPrompt("Rating: ");
+
+                        // Tạo thông tin phim mới
+                        string newMovieInfo = $"{filmid},{tenphim},{theloai},{nhasanxuat},{sotap},{luotxem},{doanhthu},{rating}";
+
+                        // Cập nhật thông tin phim trong danh sách
+                        lines[i] = newMovieInfo;
+
+                        File.WriteAllLines(dataFilePath, lines, Encoding.Unicode);
+
+                        Console.WriteLine("Dữ liệu đã được cập nhật thành công.");
+                        ShowMovieDetails(newMovieInfo); // Hiển thị thông tin chi tiết mới
+                        Console.WriteLine("Thông tin hiện tại của bộ phim sau chỉnh sửa!");
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Filmid không tồn tại trong danh sách.");
+            }
+        }
+        static void AddNewMovie()
+        {
+            Console.WriteLine("Nhập thông tin phim mới:");
+            string tenphim, theloai, nhasanxuat;
+
+            // Sử dụng Print_Prompts để hiển thị và nhập dữ liệu
+            tenphim = InputWithPrompt("Tên phim: ");
+            theloai = InputWithPrompt("Thể loại: ");
+            nhasanxuat = InputWithPrompt("Nhà sản xuất: ");
+
+            int sotap, luotxem, doanhthu;
+            double rating;
+
+            sotap = IntInputWithPrompt("Số tập: ");
+            luotxem = IntInputWithPrompt("Lượt xem: ");
+            doanhthu = IntInputWithPrompt("Doanh thu: ");
+            rating = DoubleInputWithPrompt("Rating: ");
+
+            // Tạo thông tin phim mới
+            string newMovieInfo = $"{GetNextFilmId()},{tenphim},{theloai},{nhasanxuat},{sotap},{luotxem},{doanhthu},{rating}";
+
+            // Ghi thông tin phim mới vào danh sách
+            using (StreamWriter writer = new StreamWriter(dataFilePath, true, Encoding.Unicode))
+            {
+                writer.WriteLine(newMovieInfo);
+            }
+
+            Console.WriteLine("Phim mới đã được thêm thành công.");
+            ShowMovieDetails(newMovieInfo);
+            Console.WriteLine("Đây là thông tin của bộ phim bạn vừa thêm!");
+        }
+    }
+}
---
+++ menuchinh.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    public class support
+    {
+        public static void Menuchonlua()
+        {
+            string[] menuOptions = { "Hiển thị danh sách phim", "Chỉnh sửa thông tin phim", "Thêm phim mới", "Thoát" };
+            string prompts = "Quản lý danh sách phim\n" +
+                             "Chọn tùy chọn (1/2/3/4):";
+
+            Menu menu = new Menu(menuOptions, prompts);
+            int selectedIndex = menu.Run();
+
+            ProcessSelectedOption(selectedIndex);
+
+            Console.WriteLine("Nhấn phím bất kỳ để thoát.");
+            Console.ReadKey();
+        }
+        static void ProcessSelectedOption(int selectedIndex)
+        {
+            bool check = true;
+            do
+            {
+                switch (selectedIndex)
+                {
+                    case 0:
+                        // Xử lý tùy chọn 1
+                        Console.Clear();
+                        Console.WriteLine("Bạn đã chọn Hiển thị danh sách phim.");
+                        DisplayAllMovieNames();
+                        Console.ReadLine();
+                        Menuchonlua();
+                        break;
+                    case 1:
+                        // Xử lý tùy chọn 2
+                        Console.WriteLine("Bạn đã chọn Chỉnh sửa thông tin phim.");
+                        Hienthidongian();
+
+                        int filmid;
+                        bool isNumeric = false;
+
+                        do
+                        {
+                            Console.Write("Nhập filmid của bản ghi bạn muốn chỉnh sửa: ");
+                            string input = Console.ReadLine();
+
+                            isNumeric = int.TryParse(input, out filmid);
+
+                            if (!isNumeric)
+                            {
+                                Console.WriteLine("Vui lòng nhập một số nguyên hợp lệ.");
+                            }
+                        } while (!isNumeric);
+
+                        EditMovie(filmid);
+                        Console.ReadLine();
+                        Menuchonlua();
+                        break;
+                    case 2:
+                        // Xử lý tùy chọn 3
+                        Console.Clear();
+                        Console.WriteLine("Bạn đã chọn Thêm phim mới.");
+                        AddNewMovie();
+                        Console.ReadLine();
+                        Menuchonlua();
+                        break;
+                    case 3:
+                        // Xử lý tùy chọn 4
+                        Console.WriteLine("Bạn đã chọn Thoát.");
+                        check = false;
+                        Console.WriteLine("Chương trình đang kết thúc...................");
+                        break;
+                    case 4:
+                        // lọc rác
+                        Loc_USER.Bin(filepath, accountnotify, ref gate_next, ref gate_end);
+                        Console.ReadKey(); // Dùng để nhận giá trị nhập vào từ bàn phím user
+                                           // -> Mục đích để hiện thị thông báo sau khi lọc rác thành công
+                }
+            } while (check);
+
+        }
+    }
+}
